c44a53fa86c45d33e424ee8f729b51c4b2889614
Kaloyan Maksimov <k.maksimov90@gmail.com>
2025-06-15 07:14:21 +0300

    Add React Query to persist changes from client to server

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,8 +11,6 @@ type Props = {
   openForm: (id: string) => void;
   closeForm: () => void;
   editMode: boolean;
-  submitForm: (activity: Activity) => void;
-  deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({
@@ -22,22 +20,19 @@ export default function ActivityDashboard({
   selectedActivity,
   openForm,
   closeForm,
-  editMode,
-  submitForm,
-  deleteActivity }: Props) {
+  editMode}: Props) {
   return (
     <Grid2 container spacing={3}>
       <Grid2 size={7}>
         <ActivityList
           activities={activities}
           selectActivity={selectActivity}
-          deleteActivity={deleteActivity}
         />
       </Grid2>
       <Grid2 size={5}>
         {selectedActivity && !editMode &&
           <ActivityDetail
-            activity={selectedActivity}
+            selectedActivity={selectedActivity}
             cancelSelectActivity={cancelSelectActivity}
             openForm={openForm}
           />
@@ -46,7 +41,6 @@ export default function ActivityDashboard({
          <ActivityForm 
          closeForm={closeForm} 
          activity={selectedActivity} 
-         submitForm={submitForm} 
          />}
 
       </Grid2>
